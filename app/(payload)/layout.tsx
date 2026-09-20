1592303dca771a6f6cad2712ccb49d09364dcfb8
Amr Samy <amrsamy622@gmail.com>
2026-09-20 21:50:17 +0300

    feat: add root layout for Payload CMS

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -1,12 +1,10 @@
-/* THIS FILE WAS GENERATED AUTOMATICALLY BY PAYLOAD. */
-/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
 import configPromise from '@payload-config'
 import '@payloadcms/next/css'
 import { RootLayout } from '@payloadcms/next/layouts'
 import React from 'react'
-import { serverFunction } from './serverFunction'
+import { serverFunction } from './admin/serverFunction'
 
-import { importMap } from './importMap'
+import { importMap } from './admin/importMap'
 
 type Args = {
   children: React.ReactNode
